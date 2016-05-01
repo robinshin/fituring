49a6adb62b39dbde05f697584a0cd53fb574faea
Camille Demasson <camille.demasson@gmail.com>
2016-05-01 15:31:48 +0200

    test du client serveur et ajout d'un bouton pour lancer le test

--- Android/ApplicationTest/app/src/main/java/camillepact/applicationtest/NewSongActivity.java
+++ Android/ApplicationTest/app/src/main/java/camillepact/applicationtest/NewSongActivity.java
@@ -3,11 +3,21 @@ package camillepact.applicationtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
 public class NewSongActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_song);
+        /*Client client = new Client();
+        try {
+            client.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
+
+    
 }
